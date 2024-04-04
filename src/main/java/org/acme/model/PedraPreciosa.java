2634081d374ee370fcd38063d556280202b89f8a
João Victor <joaovictorpfr@gmail.com>
2024-04-04 12:27:20 -0300

    add joia relation to PedraPreciosa

--- src/main/java/org/acme/model/PedraPreciosa.java
+++ src/main/java/org/acme/model/PedraPreciosa.java
@@ -14,12 +14,17 @@ public class PedraPreciosa extends DefaultEntity{
 
     @NotBlank(message = "Informe qual tipo de pedra")
     private Pedra pedra;
+
     @NotBlank(message = "Informe o peso da pedra")
     private String peso;
+
     @NotBlank(message = "Informe a quantidade de pedras")
     private Integer quantidade;
+
+    @NotBlank(message = "Informe qual a cor")
     private Cor cor;
-//    @ManyToOne
-//    @JoinColumn(name = "joia_id")
-//    private Joia joia;
+
+    @ManyToOne
+    @JoinColumn(name = "joia_id")
+    private Joia joia;
 }
